599ea067caf54fc2b5085a2ec2bdeab9729bd9a2
Evgenii Gafarov <evgeniy-gafarov@yandex.ru>
2022-02-15 12:29:09 +0300

    created new branch junit4

--- src/test/java/ru/netology/service/CashbackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackServiceTest.java
@@ -1,28 +1,4 @@
-package ru.netology.service;
-
-import org.testng.annotations.Test;
-
-import static org.testng.Assert.*;
-
+import static org.junit.Assert.*;
 public class CashbackServiceTest {
-    CashbackService service = new CashbackService();
-
-    @Test
-    public void shouldRemainIfMoreThan1000 () {
-        int bonus = service.remain(1500);
-        assertEquals(bonus,500);
-    }
-
-    @Test
-    public void shouldRemainIfLessThan1000 () {
-        int bonus = service.remain(900);
-        assertEquals(bonus,100);
-    }
-
-    @Test
-    public void shouldRemainIf1000 () {
-        int bonus = service.remain(1000);
-        assertEquals(bonus,0);
-    }
-
+  
 }
